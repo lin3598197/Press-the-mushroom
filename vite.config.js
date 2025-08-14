e388a54806a932beedd622a874ae2cf58f81325e
josh lin <lin3598197@gmail.com>
2025-08-14 21:10:52 +0800

    chore: set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 
+// 若部署到 GitHub Pages，base 需設為 /<REPO_NAME>/
 export default defineConfig({
+  base: '/Press-the-mushroom/',
   server: {
     port: 5173,
     open: true,
   },
+  build: {
+    outDir: 'dist'
+  }
 });
